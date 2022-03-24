d1f50af30312b87fa9c850d0c3ad0c821b63ca90
quynh25 <ttquynh25052002@gmail.com>
2022-03-24 21:21:42 +0700

    yc2

--- yêu cầu 2/3giua.js
+++ yêu cầu 2/3giua.js
@@ -1,1 +1,50 @@
-//3 ý giữa js
+//3 ý giữa jQ
+$(document).ready(function(){
+
+    function checkCountry(){
+        
+        let country = $('#country').val();
+    
+        if(country == ''){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if( checkCountry() == true){
+            $("#statusOfCountry").text('Select a country')
+            $("#statusOfCountry").css('color','red')
+        }else{
+            $("#statusOfCountry").text('Country is valid')
+            $("#statusOfCountry").css('color','blue')
+        }
+    })
+
+    function checkZipCode(){
+        
+        let zipCode = $('#zipCode').val();
+        let zipCodeRegex = /^\d+$/;
+      
+        if(zipCodeRegex.test(zipCode)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if( checkZipCode() == true){
+            $("#statusOfZipCode").text('Zip code is valid')
+            $("#statusOfZipCode").css('color','blue')
+        }else{
+            $("#statusOfZipCode").text('Zip code must be number')
+            $("#statusOfZipCode").css('color','red')
+        }
+    })
+})
