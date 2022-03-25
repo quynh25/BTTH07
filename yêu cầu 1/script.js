db2fc30ac030482edcfd788857b33f8c7241ba58
quynh25 <ttquynh25052002@gmail.com>
2022-03-25 10:05:25 +0700

    them moi

--- yêu cầu 1/script.js
+++ yêu cầu 1/script.js
@@ -1,1 +1,106 @@
 //code bằng Javascript thuần 
+//3 ý đầu js
+//1. USER ID
+let userID = document.getElementById('userID')
+let statusOfUserID = document.getElementById('statusOfUserID')
+userID.addEventListener('focus', function(){
+    this.style.border = '1px solid'
+})
+
+userID.addEventListener('focusout', removeUserID)
+
+function removeUserID(){
+    if(userID.value.length >=5 && userID.value.length <=12){
+        statusOfUserID.textContent = 'User ID is valid'
+        statusOfUserID.style.color = 'blue'
+    }
+    else{
+        statusOfUserID.textContent = 'User ID is not valid'
+        statusOfUserID.style.color = 'red'
+    }
+}
+//2.PASSWORD
+let password =document.getElementById('password')
+let statusOfPassword = document.getElementById('statusOfPassword')
+password.addEventListener('focus', function(){
+    this.style.border = '1px solid'
+})
+password.addEventListener('focusout', removePassword)
+function removePassword(){
+    if(password.value.length >=7 && password.value.length <=12){
+        statusOfPassword.textContent = 'Password is valid'
+        statusOfPassword.style.color = 'blue'
+    }
+    else{
+        statusOfPassword.textContent = 'Password is not valid'
+        statusOfPassword.style.color = 'red'
+    }
+}
+//3. Name 
+let nameOfUser = document.getElementById('name')
+let nameOfUserRegex =  /^[a-zA-Z]/;
+
+console.log(nameOfUserRegex)
+let statusOfName = document.getElementById('statusOfName')
+
+nameOfUser.addEventListener('focus', function(){
+    this.style.border = '1px solid'
+})
+nameOfUser.addEventListener('focusout', removeTextName)
+
+function removeTextName(){
+
+    if(nameOfUserRegex.test(nameOfUser.value)){
+        statusOfName.textContent = 'Your name is valid'
+        statusOfName.style.color = 'blue'
+        
+    }
+    else{
+        statusOfName.textContent = 'Your name is not valid'
+        statusOfName.style.color = 'red'
+    }
+}
+
+//5.country
+let country = document.getElementById('country')
+let statusOfCountry = document.getElementById('statusOfCountry')
+
+country.addEventListener('focus', function(){
+    this.style.border ='1px solid'
+})
+country.addEventListener('focusout', removeCountry)
+
+function removeCountry(){
+    if(country.value ==''){
+        statusOfCountry.textContent='Select a country'
+        statusOfCountry.style.color= 'red'
+
+    }
+    else{
+        statusOfCountry.textContent='Country is valid'
+        statusOfCountry.style.color= 'blue'
+
+    }
+
+}
+//6.zipCode
+let zipCode = document.getElementById('zipCode')
+let zipCodeRegex = /^\d+$/;
+let statusOfZipCode = document.getElementById('statusOfZipCode')
+
+zipCode.addEventListener('focus', function(){
+    this.style.border ='1px solid'
+})
+zipCode.addEventListener('focusout', removeZipCode)
+
+function removeZipCode(){
+    if(zipCodeRegex.test(zipCode.value)){
+        statusOfZipCode.textContent='Zip code is valid'
+        statusOfZipCode.style.color='blue'
+       
+    }
+    else{
+        statusOfZipCode.textContent='Zip code must be number'
+        statusOfZipCode.style.color='red'
+    }
+}
--- yêu cầu 2/script.js
+++ yêu cầu 2/script.js
@@ -1,1 +1,128 @@
 //Sử dụng thư viện jQuery
+//1. USER ID
+$(document).ready(function() {
+    function checkUserID() {
+        let userID = $('#userID').val();
+        let userIDRegex = /^[a-zA-Z0-9]{5,12}$/;
+        if(userIDRegex.test(userID)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if(checkUserID()){
+            $("#statusOfUserID").text('UserID is valid');
+            $("#statusOfUserID").css('color','blue')
+        }
+        else{
+            $("#statusOfUserID").text('UserID is not valid');
+            $("#statusOfUserID").css('color','red')
+        }
+    })
+})
+
+//2. PASSWORD
+$(document).ready(function() {
+    function checkPassword() {
+        let password = $('#password').val();
+        let passwordRegex = /^[a-zA-Z0-9]{7,12}$/;
+        if(passwordRegex.test(password)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if(checkPassword()){
+            $("#statusOfPassword").text('Password is valid');
+            $("#statusOfPassword").css('color','blue')
+        }
+        else{
+            $("#statusOfPassword").text('Password is not valid');
+            $("#statusOfPassword").css('color','red')
+        }
+    })
+})
+
+
+$(document).ready(function() {
+    function checkName() {
+        let name = $('#name').val();
+        let nameRegex = /^[a-zA-Z]/;
+        if(nameRegex.test(name)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if(checkName()){
+            $("#statusOfName").text('Name is valid');
+            $("#statusOfName").css('color','blue')
+        }
+        else{
+            $("#statusOfName").text('Name is not valid');
+            $("#statusOfName").css('color','red')
+        }
+    })
+})
+
+$(document).ready(function(){
+
+    function checkCountry(){
+
+        let country = $('#country').val();
+
+        if(country == ''){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if( checkCountry() == true){
+            $("#statusOfCountry").text('Select a country')
+            $("#statusOfCountry").css('color','red')
+        }else{
+            $("#statusOfCountry").text('Country is valid')
+            $("#statusOfCountry").css('color','blue')
+        }
+    })
+
+    function checkZipCode(){
+
+        let zipCode = $('#zipCode').val();
+        let zipCodeRegex = /^\d+$/;
+
+        if(zipCodeRegex.test(zipCode)){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    $("#btnRegister").click(function(e){
+        e.preventDefault();
+        if( checkZipCode() == true){
+            $("#statusOfZipCode").text('Zip code is valid')
+            $("#statusOfZipCode").css('color','blue')
+        }else{
+            $("#statusOfZipCode").text('Zip code must be number')
+            $("#statusOfZipCode").css('color','red')
+        }
+    })
+}) 
